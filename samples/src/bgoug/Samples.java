c8c57102cc3dd16083f7e6251f0022c2fd30e990
Doychin Bondzhev <doychin@dsoft-bg.com>
2016-11-11 15:16:49 +0200

    Add another stream sample.

--- samples/src/bgoug/Samples.java
+++ samples/src/bgoug/Samples.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 public class Samples {
     public static void main(String[] arg) {
         sumOfNumbersBelow100();
+
+        sumOfDistinctNumbersBelow100();
     }
 
     private static void sumOfNumbersBelow100() {
         int[] intArray = new int[]{5, 10, 22, 45, 101, 90, 200, 35, 99, 102, 45, 10, 91};
         System.out.println(Arrays.stream(intArray).filter(n -> n < 100).sum());
+    }
+
+    private static void sumOfDistinctNumbersBelow100() {
+        int[] intArray = new int[]{5, 10, 22, 45, 101, 90, 200, 35, 99, 102, 45, 10, 91};
+        System.out.println(Arrays.stream(intArray).filter(n -> n < 100).distinct().sum());
     }
 }
